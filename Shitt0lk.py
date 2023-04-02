880b6691015495abcea4fbaf3446e9a2e194c760
Christoph Gedamke <tobias_gedamke@yahoo.de>
2023-04-03 00:00:17 +0200

    Rewrite in Progress
    ~ cleaned Shitt0lk.py
    + telegram_handler.py to handle everything telegram related
    + pr0_handler.py to handle everything pr0gramm related
    - removed image_grabber.py which will become a part of pr0_handler.py
    
    current state:
    disfunctional
    + telegram bot starts up
    + prepared first handler

--- Shitt0lk.py
+++ Shitt0lk.py
@@ -1,24 +1,47 @@
 import logging
-
-# ## preparations ##
-# # load configs
-# credentials
 import telegram_handler
 
+
+# ## preparations ##
+# # start logging
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger.setLevel(logging.DEBUG)
+log_to_console = logging.StreamHandler()
+log_to_console.setLevel(logging.DEBUG)
+log_to_console.formatter = formatter
+logger.addHandler(log_to_console)
+log_to_file = logging.FileHandler(filename='sh1t.log', mode='a', encoding='utf-8')
+log_to_file.formatter = formatter
+logger.addHandler(log_to_file)
+
+'''logging.basicConfig(filename='sh1t.log',
+                    filemode='a',
+                    encoding='utf-8',
+                    level=logging.DEBUG)'''
+
+logger.debug('started')
+# # load config
+# credentials
 telegram_bot_token = open('telegramtoken', 'r').readline()
 with open('pr0token', 'r') as file:
     pr0gramm_user_name = file.readline()
     pr0gramm_password = file.readline()
-pr0gramm_flags = 15  # all flag
+# flags
+sfw = 1
+nsfw = 2
+nsfl = 4
+nsfp = 8
+pr0gramm_flags = sfw + nsfp + nsfw + nsfl  # all flags
+logger.critical(f'calculated flag(s): {pr0gramm_flags} | swf=1, nfsw=2, nsfl=4, nfsp=8')
 
-# # start logging
-logging.basicConfig(filename='sh1t.log', filemode='w', encoding='utf-8', level=logging.DEBUG)  # TODO: Read from Config
-# # prepare telegram bot
-logging.debug('starting bot')
-telegram_handler.start_bot(telegramtoken=telegram_bot_token)
-logging.info('bot started')
-# # check pr0 connection
+if __name__ == '__main__':
+    # # prepare telegram bot
+    logger.debug('starting bot')
+    telegram_handler.start_bot(telegramtoken=telegram_bot_token)
+    logger.info('bot started')
+    # # check pr0 connection
 
-# ## start ##
-# # start telegram bot (listen)
+    # ## start ##
+    # # start telegram bot (listen)
 
--- telegram_handler.py
+++ telegram_handler.py
@@ -6,24 +6,35 @@ from telegram.ext import filters
 from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler
 
 
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger.setLevel(logging.DEBUG)
+log_to_console = logging.StreamHandler()
+log_to_console.setLevel(logging.DEBUG)
+log_to_console.formatter = formatter
+logger.addHandler(log_to_console)
+log_to_file = logging.FileHandler(filename='sh1t.log', mode='a', encoding='utf-8')
+log_to_file.formatter = formatter
+logger.addHandler(log_to_file)
+
 pr0_link_filter = filters.Regex('pr0gramm\.com\/.*\/[0-9]*$')
-logging.debug(f'regex used {pr0_link_filter.pattern}')
+logger.debug(f'regex used {pr0_link_filter.pattern}')
 
 
 def start_bot(telegramtoken):
+    logger.debug('beginning of start_bot in telegram_handler')
     application = ApplicationBuilder().token(telegramtoken).build()
-    logging.debug('application created')
+    logger.debug('application created')
 
     # handlers
-    # x_handler = CommandHandler('text', function_name)
     pr0_link_message_handler = MessageHandler(pr0_link_filter, _pr0_link_)
 
     application.add_handler(pr0_link_message_handler)
-    logging.debug('telegram bot handlers added')
+    logger.debug('telegram bot handlers added')
 
-    logging.debug('start polling')
+    logger.debug('start polling')
     application.run_polling()
-    logging.info('bot up and running')
+    logger.info('bot up and running')
 
 
 async def _pr0_link_(update: Update, context: ContextTypes.DEFAULT_TYPE):
